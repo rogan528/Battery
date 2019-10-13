655824e948122ae72749dcc44659b7ca5a557470
rogan528 <3097940262@qq.com>
2019-10-13 23:50:56 +0800

    添加城市

--- app/src/main/java/com/android/battery/MainActivity.java
+++ app/src/main/java/com/android/battery/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.example.city_picker.CityListActivity;
 
 public class MainActivity extends AppCompatActivity {
     //声明AMapLocationClient类对象
     public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,17 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient = new AMapLocationClient(getApplicationContext());
         //设置定位回调监听
         mLocationClient.setLocationListener(mAMapLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(5*60*1000);
         //启动定位
         mLocationClient.startLocation();
         Log.e("zhangbin","122222222222222222");
@@ -34,8 +54,25 @@ public class MainActivity extends AppCompatActivity {
                     522);//自定义的code
         }
 
-    }
+        tv = (TextView) findViewById(R.id.tv_name);
+        Button btn = (Button) findViewById(R.id.btn_select);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CityListActivity.startCityActivityForResult(MainActivity.this);
 
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 101&& resultCode ==102){
+            String city = data.getStringExtra("city");
+            tv.setText(city);
+        }
+    }
 
     //异步获取定位结果
     AMapLocationListener mAMapLocationListener = new AMapLocationListener() {
@@ -44,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             if (amapLocation != null) {
                 if (amapLocation.getErrorCode() == 0) {
                     //解析定位结果
-                    Log.e("zhangbin","amapLocation:"+amapLocation.getCity());
+                    Log.e("zhangbin","amapLocation:"+amapLocation.getDistrict());
                 }
             }
         }
