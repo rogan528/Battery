cdbd0e5526223549450f302dd7ec111335612f5a
rogan528 <3097940262@qq.com>
2019-10-13 23:15:10 +0800

    增加电池优化代码

---
+++ app/src/main/java/com/android/battery/MainActivity.java
@@ -0,0 +1,52 @@
+package com.android.battery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+
+public class MainActivity extends AppCompatActivity {
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mAMapLocationListener);
+        //启动定位
+        mLocationClient.startLocation();
+        Log.e("zhangbin","122222222222222222");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    522);//自定义的code
+        }
+
+    }
+
+
+    //异步获取定位结果
+    AMapLocationListener mAMapLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //解析定位结果
+                    Log.e("zhangbin","amapLocation:"+amapLocation.getCity());
+                }
+            }
+        }
+    };
+}
