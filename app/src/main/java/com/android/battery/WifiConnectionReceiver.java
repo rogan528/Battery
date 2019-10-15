e691c5c8c6cb63c689faf1fed619b24b26d0b83c
rogan528 <3097940262@qq.com>
2019-10-15 23:25:54 +0800

    添加wifi广播

---
+++ app/src/main/java/com/android/battery/WifiConnectionReceiver.java
@@ -0,0 +1,17 @@
+package com.android.battery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class WifiConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (BatteryUtils.isWifi(context)){
+            Log.e("zhangbin","正在使用wifi");
+        }else {
+            Log.e("zhangbin","没有使用wifi");
+        }
+    }
+}
